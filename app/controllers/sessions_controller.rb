6325fd886a11a2e2f86b220c39cc22380ef47e9f
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-03-12 17:49:32 +0000

    Adding redirect to places after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+      @user = User.where({email: params["email"]})[0]
+      if @user
+        redirect_to "/places"
+      else
+        redirect_to "/sessions/new"
+      end
   end
 
   def destroy
