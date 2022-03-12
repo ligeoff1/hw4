5666529bce078a2b946ebadd01948d645f7d1419
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-03-12 18:35:33 +0000

    adding encryption using Bcrypt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,15 @@ class SessionsController < ApplicationController
   def create
       @user = User.where({email: params["email"]})[0]
       if @user
-        redirect_to "/places"
+        if @user.password == params["password"]
+           flash[:notice] = "Welcome!"
+           redirect_to "/places"
+        else     
+          flash[:notice] = "Incorrect Email or Password"
+          redirect_to "/sessions/new"
+        end
       else
+        flash[:notice] = "Incorrect Email or Password"
         redirect_to "/sessions/new"
       end
   end
